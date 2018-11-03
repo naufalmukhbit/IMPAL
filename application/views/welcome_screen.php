0a045f5205795664c28ffbbebb1f1bf12bbe81df
Naufal Mukhbit <nmukhbit@gmail.com>
2018-11-03 23:25:09 +0700

    Added welcome page

--- application/views/welcome_screen.php
+++ application/views/welcome_screen.php
@@ -2,8 +2,24 @@
 <html>
 <head>
 	<title>Welcome</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/welcome.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/fonts.css">
+	<!-- Scrollbar Custom CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 </head>
 <body>
-<h1>Welcome, there's nothing here yet.</h1>
+	<div class="content">
+		<div class="welcome_sign">
+			<h1>Welcome</h1>
+			<h2>Please log in or register (for new employees).</h2>
+		</div>
+		<div class="buttons mt-4">
+			<a href="#" class="btn mr-3">Log in</a>
+			<a href="#" class="btn">Register</a>
+		</div>	
+	</div>
 </body>
 </html>
