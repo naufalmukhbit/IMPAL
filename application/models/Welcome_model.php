c2548269062e1ae8909000877a8939073ffdac6c
Naufal Mukhbit <nmukhbit@gmail.com>
2018-11-03 20:15:35 +0700

    Set up a few things and added manager's sidebar template.
    Setting up the files in /config and added the MVC for a blank welcome screen. Also added the view for sidebar_manager.

---
+++ application/models/Welcome_model.php
@@ -0,0 +1,5 @@
+<?php
+class Welcome_model extends CI_Model {
+	
+}
+?>
---
+++ application/views/welcome_screen.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+</head>
+<body>
+<h1>Welcome, there's nothing here yet.</h1>
+</body>
+</html>
