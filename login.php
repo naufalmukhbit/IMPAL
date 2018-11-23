17cbfa662f87e5ba976ffa6d6e839b9c8913d81e
GerySitompul <gerytompul@gmail.com>
2018-11-24 03:26:43 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,19 +1,10 @@
-<?php
-include "koneksi.php";
-
-$login  = mysql_query("SELECT * FROM user WHERE username='$_POST[username]' AND password='$_POST[password]'");
-$ketemu = mysql_num_rows($login);
-$r      = mysql_fetch_array($login);
-
-// Apabila username dan password ditemukan
-if ($ketemu > 0){
-  session_start();
-  $_SESSION['namauser']=$r['id_user'];
-  $_SESSION['passuser']=$r['password'];
-  header('location:menuadmin.php');
-}
-else{
-  echo "<center>Login gagal! Username & password salah<br>";
-  echo "<a href=index.php><b>ULANGI LAGI</b></a></center>";
-}
+<?php 
+include 'koneksi.php';
+ 
+$username = $_POST['username'];
+$password = $_POST['password'];
+ 
+$query = mysql_query("select * from admin where username='$username' and password='$password'");
+$cek = mysql_num_rows($query);
+echo $cek;
 ?>
