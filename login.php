0d2eac901b40b8e4043d1ad00ed7d43a42de5770
GerySitompul <gerytompul@gmail.com>
2018-11-24 13:10:15 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,21 @@
-<?php 
-include 'koneksi.php';
- 
-$username = $_POST['username'];
-$password = $_POST['password'];
- 
-$query = mysql_query("select * from admin where username='$username' and password='$password'");
-$cek = mysql_num_rows($query);
-echo $cek;
+<?php
+   session_start();
+   if(isset($_SESSION['username'])) {
+   header('location:logdex.php'); }
+   require_once("logcon.php");
 ?>
+
+<title>Form Login</title>
+<div align='center'>
+  <form action="logprocess.php" method="post">
+  <h1>Masuk</h1>
+  <table>
+  <tbody>
+  <tr><td>Username</td><td> : <input name="username" type="text"></td></tr>
+  <tr><td>Password</td><td> : <input name="password" type="password"></td></tr>
+  <tr><td colspan="2" align="right"><input value="Login" type="submit"> <input value="Batal" type="reset"></td></tr>
+  <tr><td colspan="2" align="center">Belum Punya akun ? <a href="regist.php"><b>Daftar</b></a></td></tr>
+  </tbody>
+  </table>
+  </form>
+</div>
